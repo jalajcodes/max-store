362c1fce290b00929d15525ea3807da1708e102c
jalajcodes <jalaj@mail.com>
2022-02-21 14:42:09 +0530

    feat: Add Dark Mode

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-const navMenuIcon = document.querySelector(".nav__menu-icon");
-const navLinks = document.querySelector(".nav__links");
-
-if (navLinks && navMenuIcon) {
-  navMenuIcon.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-}
